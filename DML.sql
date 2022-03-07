c0cee72265a553721ae9b1e187fc07fb0ea5f648
Vedakeerthi A <77675280+Vedakeerthi@users.noreply.github.com>
2022-03-07 22:16:16 +0530

    Updated Data manipulation statements

--- DML.sql
+++ DML.sql
@@ -2,6 +2,7 @@ use student;
 
 create table employee(sname varchar(20), roll int, dept varchar(30));
 
+# insert statement
 insert into employee values('Veda', 08, 'CSE');
 insert into employee values('Shivi', 07, 'CSE');
 insert into employee values('Sharan', 06, 'CSE');
@@ -15,12 +16,15 @@ desc employee;
 
 update employee set dept='CSE';
 
+# select statement
 select * from employee;
 
+# delete statement
 delete from employee where roll=8;
 
 select * from employee;
 
+# truncate statement
 truncate table employee;
 
 select * from employee;
