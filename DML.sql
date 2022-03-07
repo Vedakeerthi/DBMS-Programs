12b9a8718886504de5e79f6375f551aae72d3b66
Vedakeerthi A <77675280+Vedakeerthi@users.noreply.github.com>
2022-03-07 20:44:42 +0530

    Initial commite

---
+++ DML.sql
@@ -0,0 +1,26 @@
+use student;
+
+create table employee(sname varchar(20), roll int, dept varchar(30));
+
+insert into employee values('Veda', 08, 'CSE');
+insert into employee values('Shivi', 07, 'CSE');
+insert into employee values('Sharan', 06, 'CSE');
+insert into employee values('Kamal', 05, 'CSE');
+
+select * from employee;
+
+alter table employee add(languages varchar(10), grade varchar(3));
+
+desc employee;
+
+update employee set dept='CSE';
+
+select * from employee;
+
+delete from employee where roll=8;
+
+select * from employee;
+
+truncate table employee;
+
+select * from employee;
